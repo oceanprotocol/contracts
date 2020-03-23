ffe9bb9aaf7b4b2736617a0cc7f8568d953d508c
arsenyjin <kinspotting@protonmail.com>
2020-03-23 12:56:15 +0100

    add base structure

--- contracts/DataToken.sol
+++ contracts/DataToken.sol
@@ -1,7 +1,35 @@
 pragma solidity ^0.5.3;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
 
-contract DataToken is ERC20 {
-	
+contract DataToken is ERC20,Initializable{
+
+	uint8 constant DECIMALS = 18;
+
+    string  name;
+    string  symbol;
+    // uint8   decimals;
+	string  metadata;
+	// address owner;
+
+	function initialize(
+		string memory  _name, 
+		string memory _symbol, 
+		string memory _metadata
+		// uint8   _decimals
+		) public initializer 
+	{
+		// require(owner == address(0),
+		// 	"owner should be a zero address");
+
+		name 	 = _name;
+		symbol 	 = _symbol;
+		metadata = _metadata;
+
+		// decimals = DECIMALS;	
+		// owner 	 = msg.sender;
+		// Ownable.initialize(msg.sender);
+	} 
+
 }
--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -3,6 +3,31 @@ pragma solidity ^0.5.3;
 import '@openzeppelin/upgrades/contracts/upgradeability/ProxyFactory.sol';
 
 contract TokenFactory is ProxyFactory {
+
+	address public template;
 	
+	address[] tokens;
+	mapping(string => uint256) indicies;
+
+	constructor(address _template) public {
+    	template = _template;
+  	}
+
+  	function createToken(
+		string calldata  _name, 
+		string calldata _symbol, 
+		string calldata _metadata
+	) 
+	external 
+	returns(address  token) 
+	{
+        bytes memory _payload = abi.encodeWithSignature("initialize(string,string,string)", _name, _symbol, _metadata);
+		token = deployMinimal(template, _payload);
+
+		// tokens.push(token);
+
+	}
+
+
 }
 
