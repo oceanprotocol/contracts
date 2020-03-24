22b19310e8399ae4993aa432b7a77fac9255522a
arsenyjin <kinspotting@protonmail.com>
2020-03-24 16:04:50 +0100

    add minimal registry and todo

--- contracts/DataToken.sol
+++ contracts/DataToken.sol
@@ -1,35 +1,57 @@
 pragma solidity ^0.5.3;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/upgrades/contracts/Initializable.sol';
+
+// TODO
+// [] ERC20 standard interface + metadata
+// [] DataToken is an integer(not divisible)
+// [] add Ownable
+// [] autogenerates human readable token names(ex. OceanDataToken1 - OceanDataTokenN)
+// [] Implement dynamic fees for 'mint', 'transfer', 'approve', Deploy proxy contract 
 
 contract DataToken is ERC20, Initializable{
 
-	uint8 constant DECIMALS = 18;
-
     string  name;
     string  symbol;
-    // uint8   decimals;
 	string  metadata;
-	// address owner;
 
 	function initialize(
 		string memory  _name, 
 		string memory _symbol, 
 		string memory _metadata
-		// uint8   _decimals
 		) public initializer 
 	{
-		// require(owner == address(0),
-		// 	"owner should be a zero address");
 
 		name 	 = _name;
 		symbol 	 = _symbol;
 		metadata = _metadata;
 
-		// decimals = DECIMALS;	
-		// owner 	 = msg.sender;
 		// Ownable.initialize(msg.sender);
 	} 
+
+	function _mint(address to, address payable from)
+        external
+        payable
+    {
+    	// require(owner == msg.sender,
+    	// 	"minter should be a message sender");
+        uint256 startGas = gasleft();
+        
+        super._mint(address(this), 1);
+        
+        uint256 usedGas = startGas - gasleft();
+        uint256 fee = usedGas * tx.gasprice;
+
+    	require(msg.value > fee,
+    		"not enough ether to deduct fee");
+
+        if (msg.value > fee){
+        	from.transfer(msg.value-fee);
+        }
+
+        _transfer(address(this), to, 1);
+    }
+
 
 }
--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -1,13 +1,20 @@
 pragma solidity ^0.5.3;
 
 import '@openzeppelin/upgrades/contracts/upgradeability/ProxyFactory.sol';
+import './DataToken.sol';
+
+// TODO
+// [] 'Uniswap-like architecture'
+// []  minimal token registry
+// []  token registry related functions
 
 contract TokenFactory is ProxyFactory {
 
 	address public template;
 	
 	address[] tokens;
-	mapping(string => uint256) ids;
+   	mapping (uint256 => address) idToToken; 
+   	mapping (address => uint256) tokenToId;
 
 	constructor(address _template) public {
     	template = _template;
@@ -19,19 +26,23 @@ contract TokenFactory is ProxyFactory {
 		string calldata _metadata
 	) 
 	external 
-	// returns(address  token) 
 	{
-        require(ids[_symbol] == 0,
-        	"token symbol already exists");
-        
+
         bytes memory _payload = abi.encodeWithSignature("initialize(string,string,string)", _name, _symbol, _metadata);
 		address token = deployMinimal(template, _payload);
 
 		tokens.push(token);
-		ids[_symbol] = tokens.length; 
-
+		idToToken[tokens.length] = token;
+		tokenToId[token] = tokens.length;
 	}
 
+	function getTokenAddress(uint256 tokenId) public view returns(address) {
+		return idToToken[tokenId];
+	}
+
+	function getTokenId(address tokenAddress) public view returns(uint256) {
+		return tokenToId[tokenAddress];
+	}
 
 }
 
