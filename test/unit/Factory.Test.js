4fac3d9e36cb8abfa509aa64b5635a40909c17e3
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2020-05-04 15:24:38 +0200

    fix solidity coverage

--- test/unit/Factory.Test.js
+++ test/unit/Factory.Test.js
@@ -2,10 +2,10 @@ const Factory = artifacts.require("Factory");
 
 const truffleAssert = require('truffle-assertions');
 
-contract("actory test", async accounts => {
+contract("factory test", async accounts => {
   let factory;
 
-  beforeEach('innit contracts for each test', async function () {
+  beforeEach('init contracts for each test', async function () {
 
   	factory = await Factory.new(accounts[0]);
 
