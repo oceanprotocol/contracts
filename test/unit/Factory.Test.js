136a0c0b47acb678b2b357e1d85d7bde5a415cfd
arsenyjin <kinspotting@protonmail.com>
2020-05-04 19:10:34 +0200

    wip more tests

--- test/unit/Factory.Test.js
+++ test/unit/Factory.Test.js
@@ -1,19 +1,33 @@
 const Factory = artifacts.require("Factory");
+const Template = artifacts.require("ERC20Template");
+const FeeManager = artifacts.require("FeeManager");
 
 const truffleAssert = require('truffle-assertions');
 
-contract("factory test", async accounts => {
+contract("Factory test", async accounts => {
+  let feeManager;
+  let templete;
   let factory;
+  let result;
+  let tokenAddress;
 
   beforeEach('init contracts for each test', async function () {
-
-  	factory = await Factory.new(accounts[0]);
+  	feeManager = await FeeManager.new();
+  	template = await Template.new("Template Contract", "TEMPLATE", accounts[0], feeManager.address); 
+  	factory = await Factory.new(template.address);
 
   })
 
-  it("should create a token", async () => {
+  it("should create a token and check that it's not a zero address", async () => {
     
-    truffleAssert.passes(factory.createToken("logic", "TestDataToken", "TDT", accounts[0]));
+    truffleAssert.passes(
+    	result = await factory.createToken("logic", "TestDataToken", "TDT", accounts[0])
+    );
+
+	truffleAssert.eventEmitted(result, 'TokenCreated', (ev) => {
+		tokenAddress = ev.param1;
+		return tokenAddress != "0x0000000000000000000000000000000000000000";
+	});
 
   });
 
--- test/unit/Token.Test.js
+++ test/unit/Token.Test.js
@@ -1,20 +1,30 @@
+const Template = artifacts.require("ERC20Template");
+const FeeManager = artifacts.require("FeeManager");
 const Factory = artifacts.require("Factory");
+const Token = artifacts.require("ERC20Template");
 
 const truffleAssert = require('truffle-assertions');
 
-contract("Token test", async accounts => {
+contract("Factory test", async accounts => {
   let factory;
+  let result;
+  let token;
+  let tokenAddress;
 
-  beforeEach('innit contracts for each test', async function () {
+  beforeEach('init contracts for each test', async function () {
 
-  	factory = await Factory.new(accounts[0]);
+    feeManager = await FeeManager.new();
+    template = await Template.new("TestToken", "TEST", accounts[0], feeManager.address);
+  	factory = await Factory.new(template.address);
+  	result = await factory.createToken("logic", "TestDataToken", "TDT", accounts[0]);
+  	tokenAddress = await factory.currentTokenAddress();
+  	token = await Token.at(tokenAddress);     
 
   })
 
-  it("should create a token", async () => {
-    
-    truffleAssert.passes(factory.createToken("logic", "TestDataToken", "TDT", accounts[0]));
-
+  it("should check that the token is initialized", async () => {
+    const isInitialized = await token.isInitialized();
+    // assert(isInitialized == true);
   });
 
 });
