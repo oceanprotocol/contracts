53b6debc699b3b0a64b310abe84c9123f8797f7e
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2020-05-15 11:22:42 +0200

    fix lint errors

--- contracts/fee/FeeCalculator.sol
+++ contracts/fee/FeeCalculator.sol
@@ -3,13 +3,12 @@ pragma solidity ^0.5.7;
 // SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 // Code is Apache-2.0 and docs are CC-BY-4.0
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract FeeCalculator {
 
     using SafeMath for uint256;
     uint256 constant private BASE_TX_COST = 44000;
-    uint256 constant private FACTOR_1 = 10;
     uint256 constant private BASE = 10;
     
     function calculateRange(
@@ -21,9 +20,9 @@ contract FeeCalculator {
     {
         uint256 remainder = number;
         uint256 zeros = 0;
-        for(uint256 i=0 ; remainder >= BASE; i++){
+        for(uint256 i = 0 ; remainder >= BASE; i++){
             remainder = remainder.div(BASE);
-            zeros +=1 ;
+            zeros += 1;
         }
         return zeros;
     }
@@ -44,7 +43,8 @@ contract FeeCalculator {
         
         uint256 tokensRange = calculateRange(tokens);
         uint256 tokensRangeToll = tokensRange.mul(BASE_TX_COST);
-        uint256 toll = tokensRangeToll.div(calculateRange(totalSupply)).div(FACTOR_1);
-        return toll;
+        return tokensRangeToll.div(
+                calculateRange(totalSupply)
+            ).div(BASE);
     }
 }
