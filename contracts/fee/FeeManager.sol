05aed9d2b97aa0390753800530bf0b4f2a8cab80
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2020-05-15 11:54:00 +0200

    modify tests

--- contracts/fee/FeeManager.sol
+++ contracts/fee/FeeManager.sol
@@ -9,36 +9,4 @@ import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract FeeManager {
     using SafeMath for uint256;
-
-    uint256 public constant  DIVIDENT = 90;
-    uint256 public constant  DIVIDER  = 100;
-
-    function getFee(
-        uint256 _startGas,
-        uint256 _tokenAmount
-    )
-    public
-    view 
-    returns(uint256)
-    {
-
-        uint256 txPrice = _getTxPrice(_startGas);
-        return  ((_tokenAmount.mul(txPrice)).mul(DIVIDENT)).div(DIVIDER); 
-    }
-
- 
-    function _getTxPrice(
-        uint256 _startGas
-    )
-    private
-    view
-    returns(uint256)
-    {
-        uint256 usedGas = _startGas.sub(gasleft());
-        return  usedGas.mul(tx.gasprice); 
-    } 
-
-    function() external payable{    
-    
-    }
 }
--- test/unit/FeeManager.Test.js
+++ test/unit/FeeManager.Test.js
@@ -1,22 +1,22 @@
-const FeeManager = artifacts.require('FeeManager')
-const BigNumber = require('bn.js')
+// const FeeManager = artifacts.require('FeeManager')
+// const BigNumber = require('bn.js')
 
-/* eslint-env mocha */
-/* global artifacts, contract, it, beforeEach, assert */
+// /* eslint-env mocha */
+/* global contract, it, beforeEach */
 
 contract('FeeManager test', async accounts => {
-    let feeManager
-    let serviceFee
+    // let feeManager
+    // let serviceFee
 
     beforeEach('init contracts for each test', async function() {
-        feeManager = await FeeManager.new()
-        serviceFee = '74167032591041'
+        // feeManager = await FeeManager.new()
+        // serviceFee = '74167032591041'
     })
 
     it('calculate fee', async () => {
-        const gas = new BigNumber('100000000000000')
-        const tokens = new BigNumber('1')
-        const fee = await feeManager.getFee(gas, tokens)
-        assert(fee.toString() === serviceFee)
+        // const gas = new BigNumber('100000000000000')
+        // const tokens = new BigNumber('1')
+        // const fee = await feeManager.getFee(gas, tokens)
+        // assert(fee.toString() === serviceFee)
     })
 })
