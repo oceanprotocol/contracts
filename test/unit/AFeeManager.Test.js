248955f3e0c5656e411d655e88d4d08a2e64425b
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2020-05-26 17:39:43 +0200

    Fee manager coverage

--- test/unit/AFeeManager.Test.js
+++ test/unit/AFeeManager.Test.js
@@ -1,5 +1,5 @@
 /* eslint-env mocha */
-/* global contract, it, beforeEach, assert */
+/* global contract, it, beforeEach, assert, artifacts */
 
 const FeeManager = artifacts.require('FeeManager')
 const utils = require('../helpers/utils')
@@ -8,11 +8,14 @@ const BigNumber = require('bn.js')
 contract('FeeManager', async (accounts) => {
     let feeManager,
         sender,
-        web3
+        owner,
+        web3,
+        value
     beforeEach('init contracts for each test', async () => {
-        sender = accounts[0]
-        value = new BigNumber('100000000000000000')
-        feeManager = await FeeManager.new()
+        owner = accounts[0]
+        sender = accounts[1]
+        value = new BigNumber('5000000000000000000')
+        feeManager = await FeeManager.new({from: owner})
         web3 = await utils.getWeb3()
     })
 
@@ -25,8 +28,36 @@ contract('FeeManager', async (accounts) => {
         )
         await web3.eth.getBalance(feeManager.address)
             .then((balance) => {
-                console.log(balance, value)
-                assert(value == balance)
+                assert(new BigNumber(balance), value)
             })
+    })
+
+    it('should owner withdraw Eth from fee manager contract', async() => {
+        let ownerBalanceAfter, 
+        ownerBalanceBefore
+        await web3.eth.getBalance(owner)
+        .then((ownerBalance) => {
+            ownerBalanceBefore = ownerBalance
+        })
+        
+        await feeManager.send(
+            value,
+            {
+                from: sender
+            }
+        )
+        await feeManager.withdraw(
+            {
+                from: owner
+            }
+        )
+        await web3.eth.getBalance(owner)
+        .then((ownerBalance) => {
+            ownerBalanceAfter = ownerBalance
+        })
+        
+        assert(
+            new BigNumber(ownerBalanceBefore) < new BigNumber(ownerBalanceAfter)
+        )
     })
 })
