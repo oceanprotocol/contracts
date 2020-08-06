6c5865d74830319d69d09d29f81f25f4a76702e3
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2020-08-06 12:26:12 +0200

    add swap

--- contracts/fixedRate/FixedRateExchange.sol
+++ contracts/fixedRate/FixedRateExchange.sol
@@ -3,14 +3,13 @@ pragma solidity ^0.5.7;
 // SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 // Code is Apache-2.0 and docs are CC-BY-4.0
 
-import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import '../interfaces/IERC20Template.sol';
 
 /**
  * @title FixedRateExchange
  * @dev FixedRateExchange is a fixed rate exchange Contract
  */
-contract FixedRateExchange is Ownable {
+contract FixedRateExchange {
 
     struct FixedRatePool {
         address poolOwner;
@@ -21,54 +20,76 @@ contract FixedRateExchange is Ownable {
 
     // map a poolId to a fixedRatePool
     mapping(bytes32 => FixedRatePool) fixedRatePools;
+    bytes32[] poolIds;
 
-    modifier onlyNotExistPool(
-        address dataTokenAddress,
-        address baseTokenAddress
+    modifier onlyExistPool(
+        address baseToken,
+        address dataToken
     )
     {
-        bytes32 _id = keccak256(
-            abi.encodePacked(
-                dataTokenAddress,
-                baseTokenAddress
-            )
+        bytes32 id = getPoolId(
+            baseToken,
+            dataToken
         );
         require(
-            fixedRatePools[_id].fixedRate == 0,
-            'FixedRateExchange: Pool already exists!'
+            fixedRatePools[id].fixedRate != 0,
+            'FixedRateExchange: Pool does not exist!'
         );
         _;
     }
 
+    event PoolCreated(
+        address indexed poolOwner,
+        address indexed baseToken,
+        address indexed dataToken,
+        uint fixedRate
+    );
 
-    constructor() 
-        public
-        Ownable()
+
+    event Swaped(
+        bytes32 indexed poolId,
+        address indexed by,
+        uint256 baseTokenSwapedAmount,
+        uint256 dataTokenSwapedamount
+    );
+
+
+    constructor () public {}
+
+    function getPools()
+        external 
+        view 
+        returns (bytes32[] memory)
     {
+        return poolIds;
     }
 
-
     function createPool(
-        address dataTokenAddress,
-        address baseTokenAddress,
+        address baseToken,
+        address dataToken,
         uint256 fixedRate
     )
-        public
-        onlyNotExistPool(
-            dataTokenAddress,
-            baseTokenAddress
-        )
+        external
     {
+        bytes32 id = getPoolId(
+            baseToken,
+            dataToken
+        );
         require(
-            baseTokenAddress != address(0),
+            fixedRatePools[id].fixedRate == 0,
+            'FixedRateExchange: Pool already exists!'
+        );
+
+        require(
+            baseToken != address(0),
             'FixedRateExchange: Invalid basetoken,  zero address'
         );
         require(
-            dataTokenAddress != address(0),
+            dataToken != address(0),
             'FixedRateExchange: Invalid datatoken,  zero address'
         );
         require(
-            baseTokenAddress != dataTokenAddress,
+            baseToken != dataToken,
             'FixedRateExchange: Invalid datatoken,  equals basetoken'
         );
         require(
@@ -76,107 +97,87 @@ contract FixedRateExchange is Ownable {
             'FixedRateExchange: Invalid exchange rate value'
         );
 
-        bytes32 _id = keccak256(
-            abi.encodePacked(
-                dataTokenAddress,
-                baseTokenAddress
-            )
-        );
-
-        fixedRatePools[_id] = FixedRatePool({
+        fixedRatePools[id] = FixedRatePool({
             poolOwner: msg.sender,
-            dataToken: dataTokenAddress,
-            baseToken: baseTokenAddress,
+            dataToken: dataToken,
+            baseToken: baseToken,
             fixedRate: fixedRate
         });
+        poolIds.push(id);
+
+        emit PoolCreated(
+            msg.sender,
+            baseToken,
+            dataToken,
+            fixedRate
+        );
     }
 
-    // /**
-    //  * @dev isInitialized
-    //  *      Function checks if the contract is initialized.
-    //  * @return true if the contract is initialized, false if it is not.
-    //  */
+    function getPoolId(
+        address baseToken,
+        address dataToken
+    )
+        public
+        returns (bytes32)
+    {
+        return keccak256(
+            abi.encodePacked(
+                baseToken,
+                dataToken
+            )
+        );
+    }
 
-    // function isInitialized() public view returns (bool) {
-    //     return initialized;
-    // }
+    function swap(
+        address baseToken,
+        address dataToken,
+        uint dataTokenAmount
+    )
+        external
+        onlyExistPool(
+            baseToken,
+            dataToken
+        )
+    {
+        bytes32 id = getPoolId(
+            baseToken,
+            dataToken
+        );
+        uint256 baseTokenAmount = 
+            dataTokenAmount * fixedRatePools[id].fixedRate ;
+        require(
+            IERC20Template(baseToken).transfer(
+                fixedRatePools[id].poolOwner, 
+                baseTokenAmount
+            ),
+            'FixedRateExchange: transfer failed in the baseToken contract'
+        );
+        require(
+            IERC20Template(dataToken).transferFrom(
+                fixedRatePools[id].poolOwner,
+                msg.sender,
+                dataTokenAmount
+            ),
+            'FixedRateExchange: transferFrom failed in the dataToken contract'
+        );
 
-    // /**
-    //  * @dev buyDataTokens
-    //  *      Buys Datatokens using base token
-    //  * @param dtAmount amount of DataTokens to be bought
-    //  * @return true
-    //  */
-    // function buyDataTokens(uint256 dtAmount) public 
-    // onlyInitialized returns (bool) {
-    //     //TO DO - This assumes that ratio is going to be always expressed in wei
-    //     uint256 baseAmount = dtAmount * (_ratio / (10**18));
-    //     //TO DO  - should we check the reserve first or just let it fail if there is not enough DT ?
-    //     require(
-    //         IERC20Template(_basetoken).transfer(_lpAddress, baseAmount),
-    //         'ERROR: transfer failed'
-    //     );
-    //     require(
-    //         IERC20Template(_datatoken).transferFrom(
-    //             _lpAddress,
-    //             msg.sender,
-    //             dtAmount
-    //         ),
-    //         'ERROR: transferFrom failed'
-    //     );
-    //     return true;
-    // }
+        emit Swaped(
+            id,
+            msg.sender,
+            baseTokenAmount,
+            dataTokenAmount
+        );
+    }
 
-    // /**
-    //  * @dev getRatio
-    //  *      Gets Ratio
-    //  * @return uint ratio
-    //  */
-    // function getRatio() public view onlyInitialized returns (uint256) {
-    //     return (_ratio);
-    // }
+    function getNumberOfPools()
+        external
+        view
+        returns (uint256)
+    {
+        return poolIds.length;
+    }
 
-    // /**
-    //  * @dev SetRatio
-    //  *      Sets a new Ratio
-    //  * @return uint ratio
-    //  */
-    // function setRatio(uint ratio) public 
-    // onlyOwner onlyInitialized returns (bool) {
-    //     require(ratio>0,'Ratio must be >0');
-    //     uint oldratio = _ratio;
-    //     _ratio = ratio;
-    //     emit RatioChanged(oldratio,ratio);
-    //     return true;
-
-    // }
-
-    // /**
-    //  * @dev getTokens
-    //  *      Gets tokens addresses
-    //  * @return address[] tokens
-    //  */
-    // function getTokens() public view 
-    // onlyInitialized returns (address[] memory) {
-    //     address[] memory tokens = new address[](2);
-    //     tokens[0] = _basetoken;
-    //     tokens[1] = _datatoken;
-    //     return (tokens);
-    // }
-
-    // /**
-    //  * @dev getDTReserve
-    //  *      Gets amount of DT available to trade
-    //  * @return uint amount of DT
-    //  */
-    // function getDTReserve() public view onlyInitialized returns (uint256) {
-    //     //get both balance & allowence and return the smaller one
-    //     uint256 balance = IERC20Template(_datatoken).balanceOf(_lpAddress);
-    //     uint256 allowance = IERC20Template(_datatoken).allowance(
-    //         _lpAddress,
-    //         address(this)
-    //     );
-    //     if (balance < allowance) return (balance);
-    //     else return (allowance);
-    // }
+    // TODO: 
+    // set rate
+    // get rate
 }
