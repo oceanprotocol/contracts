e0482973cedb0ec86cf3813dac089c7676585e6e
alexcos20 <alex.coseru@gmail.com>
2020-08-28 03:01:38 -0700

    draft

---
+++ contracts/CommunityFeeCollector/CommunityFeeCollector.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.7;
+// Copyright BigchainDB GmbH and Ocean Protocol contributors
+// SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+// Code is Apache-2.0 and docs are CC-BY-4.0
+import '../interfaces/IERC20Template.sol';
+//import "@openzeppelin/contracts/ownership/Ownable.sol";
+import '../utils/Ownable.sol';
+
+contract CommunityFeeCollector is Ownable{
+    address payable private collector;
+    
+
+  constructor(
+      address payable newCollector
+  ) public {
+      collector = newCollector;
+  }
+
+  function () external payable { }
+
+  function redrawETH() public payable {
+    collector.transfer(address(this).balance);
+  }
+
+  function redrawToken(address tokenaddress) public payable{
+    IERC20Template(tokenaddress).transfer(collector,IERC20Template(tokenaddress).balanceOf(address(this)));
+  }
+
+  function changeCollector(address payable newCollector) public onlyOwner{
+      require(newCollector != address(0),'New collector should not be 0');
+      collector = newCollector;
+  }
+
+}
