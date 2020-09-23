c12699344887f8fc5e440cfae7bd7372d8629eee
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2020-09-23 15:29:57 +0200

    fix DPL-01, DPL-02

--- contracts/utils/Deployer.sol
+++ contracts/utils/Deployer.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.7;
+pragma solidity 0.5.7;
 // Copyright BigchainDB GmbH and Ocean Protocol contributors
 // SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 // Code is Apache-2.0 and docs are CC-BY-4.0
@@ -32,6 +32,7 @@ contract Deployer {
     {
         bytes20 targetBytes = bytes20(_logic);
         /* solium-disable-next-line security/no-inline-assembly */
+        // Adapted from https://github.com/optionality/clone-factory/blob/32782f82dfc5a00d103a7e61a17a5dedbd1e8e9d/contracts/CloneFactory.sol
         assembly {
           let clone := mload(0x40)
           mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
