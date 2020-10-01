038a55863ad193cace5a8781f68fbfab80208f28
Alex Coseru <alex.coseru@gmail.com>
2020-10-01 16:29:02 +0300

    Update DataTokenTemplate.sol

--- contracts/templates/DataTokenTemplate.sol
+++ contracts/templates/DataTokenTemplate.sol
@@ -44,7 +44,8 @@ contract DataTokenTemplate is IERC20Template, ERC20 {
             address indexed consumer, 
             uint256 amount, 
             uint256 serviceId, 
-            address indexed provider
+            address indexed provider,
+            uint256 stoppedAt
     );
 
     event MinterProposed(
@@ -241,7 +242,7 @@ contract DataTokenTemplate is IERC20Template, ERC20 {
             msg.sender,
             amount,
             serviceId,
-            block.number,
+            block.timestamp,
             mrktFeeCollector,
             marketFee
         );
@@ -276,7 +277,8 @@ contract DataTokenTemplate is IERC20Template, ERC20 {
             consumer, 
             amount, 
             serviceId, 
-            msg.sender
+            msg.sender,
+            block.timestamp
         );
     }
 
