ffc63b39678ee554e06600e1b9159a17eb973b20
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-04-05 15:52:22 +0200

    clean up setup.py

--- setup.py
+++ setup.py
@@ -2,28 +2,15 @@
 # -*- coding: utf-8 -*-
 
 """The setup script."""
-
-from setuptools import setup
-import os
 from glob import glob
-
+from setuptools import setup,find_packages
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
 requirements = []
-
 setup_requirements = []
-
-
 test_requirements = []
-artifact_folder = './artifacts'
-print("Adding all files in /{}".format(artifact_folder))
-data_files = [(artifact_folder, [f for f in glob(os.path.join(artifact_folder, '*'))])]
-
-print("data_files=")
-for df in data_files:
-    print(df)
 
 setup(
     author="leucothia",
@@ -38,7 +25,7 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     description=" 🐳 Ocean Protocol L1 - v3",
-    data_files=data_files,
+    packages=find_packages(),
     install_requires=requirements,
     license="Apache Software License 2.0",
     long_description=readme,
