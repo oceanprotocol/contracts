a2d4771395225db749eba645683ef3fced735a25
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-04-05 09:32:50 +0200

    include __init__.py file in py package

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -4,7 +4,7 @@ pip install --upgrade --user travis pip
 pip install --upgrade --user travis twine six==1.10.0 wheel==0.31.0 setuptools
 pip list
 shopt -s nullglob
-abifiles=( ./artifacts/*.json )
+abifiles=( ./artifacts/* )
 [ "${#abifiles[@]}" -lt "1" ] && echo "ABI Files for development environment not found" && exit 1
 python setup.py sdist bdist_wheel
 twine upload dist/*
