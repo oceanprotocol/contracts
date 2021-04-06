674e18545ca030aeea1c7f19f704f782d71a5a9f
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-04-06 12:53:15 +0200

    v0.5.13

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-pip3 install --upgrade --user travis pip3
-pip3 install --upgrade pip
-pip3 install --upgrade --user travis setuptools-rust twine six==1.10.0 wheel==0.31.0 setuptools
+#pip3 install --upgrade --user travis pip
+pip3 install --upgrade travis pip
+#pip3 install --upgrade --user travis setuptools-rust twine six==1.10.0 wheel==0.31.0 setuptools
+pip3 install --upgrade twine six==1.10.0 wheel==0.31.0 setuptools
+pip3 show twine
 pip3 list
 shopt -s nullglob
 abifiles=( ./artifacts/* )
