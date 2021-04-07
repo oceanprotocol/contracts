1922be2587cccd32337860600be33828d14ace11
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-04-07 12:21:30 +0200

    v0.5.13

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#pip3 install --upgrade --user travis pip
-pip3 install --upgrade travis pip
-#pip3 install --upgrade --user travis setuptools-rust twine six==1.10.0 wheel==0.31.0 setuptools
-pip3 install --upgrade twine six==1.10.0 wheel==0.31.0 setuptools
-pip3 show twine
-pip3 list
+pip install --upgrade --user travis pip
+pip install --upgrade --user travis twine six==1.10.0 wheel==0.31.0 setuptools
+pip list
 shopt -s nullglob
 abifiles=( ./artifacts/* )
 [ "${#abifiles[@]}" -lt "1" ] && echo "ABI Files for development environment not found" && exit 1
