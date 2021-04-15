8dc5382e31b00788c6f07c2023dd1468f3435f6d
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-04-15 15:00:41 +0200

    v0.5.13

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+pip install --upgrade --user travis pip
+pip install --upgrade --user travis twine six==1.10.0 wheel==0.31.0 setuptools
+pip list
 shopt -s nullglob
 abifiles=( ./artifacts/* )
 [ "${#abifiles[@]}" -lt "1" ] && echo "ABI Files for development environment not found" && exit 1
-python3 setup.py sdist bdist_wheel
+python setup.py sdist bdist_wheel
 twine upload dist/*
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 """The setup script."""
 from glob import glob
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
