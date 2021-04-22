796f22729f6e6a4f92e659ca5197e9000bc7911a
lacoop6tu <burzi.andrea@gmail.com>
2021-04-22 16:18:25 -0500

    add support for multiple ERC721Templates (mapping)

--- contracts/ERC721Factory.sol
+++ contracts/ERC721Factory.sol
@@ -1,4 +1,5 @@
 pragma solidity >=0.6.0;
+pragma experimental ABIEncoderV2;
 // Copyright BigchainDB GmbH and Ocean Protocol contributors
 // SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 // Code is Apache-2.0 and docs are CC-BY-4.0
@@ -6,6 +7,7 @@ pragma solidity >=0.6.0;
 import './utils/Deployer.sol';
 import './interfaces/IERC721Template.sol';
 import './interfaces/IERC20Factory.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
 /**
  * @title DTFactory contract
  * @author Ocean Protocol Team
@@ -16,11 +18,21 @@ import './interfaces/IERC20Factory.sol';
  *      New DataToken proxy contracts are links to the template contract's bytecode.
  *      Proxy contract functionality is based on Ocean Protocol custom implementation of ERC1167 standard.
  */
-contract ERC721Factory is Deployer {
+contract ERC721Factory is Deployer, Ownable {
     address private tokenTemplate;
     address private communityFeeCollector;
     uint256 private currentTokenCount = 1;
     address private erc20Factory;
+    uint public templateCount;
+    
+    struct Template {
+        address templateAddress;
+        bool isActive;
+    }
+
+    
+
+    mapping(uint => Template) public templateList;
 
     event TokenCreated(
         address indexed newTokenAddress,
@@ -38,6 +50,7 @@ contract ERC721Factory is Deployer {
         string indexed blob
     );
 
+    
     /**
      * @dev constructor
      *      Called on contract deployment. Could not be called with zero address parameters.
@@ -121,10 +134,27 @@ contract ERC721Factory is Deployer {
      * @dev get the token template address
      * @return the template address
      */
-    function getTokenTemplate() external view returns (address) {
-        return tokenTemplate;
+    function getTokenTemplate(uint _index) external view returns (Template memory) {
+        Template memory template = templateList[_index];
+        return template;
     }
 
+
+    function addTokenTemplate(address _templateAddress) external onlyOwner returns (uint){
+           templateCount += 1;
+           Template memory template = Template(_templateAddress,true);
+           templateList[templateCount] = template;
+           return templateCount;
+    }
+
+    function disableTokenTemplate(uint _index) external onlyOwner {
+        Template storage template = templateList[_index];
+        template.isActive = false;
+    }
+
+    function getCurrentTemplateCount() external view returns (uint256) {
+        return templateCount;
+    }
     receive() external payable{
 
     }
