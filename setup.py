5a9039a36421851952ad2089e8d3602dc210f113
alexcos20 <alex.coseru@gmail.com>
2021-04-22 00:56:33 -0700

    v0.5.13

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/ocean-contracts',
-    version='0.5.15',
+    version='0.5.16',
     zip_safe=False,
 )
