03f45d40d519646230f972e0dd2bfbc8604ff4ce
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-04-26 12:02:26 +0200

    minor fix

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -5,7 +5,7 @@ pip install --upgrade --user travis twine six==1.10.0 wheel==0.31.0 setuptools
 pip list
 shopt -s nullglob
 mkdir -p ocean_contracts/artifacts
-mv ./artifacts/* ocean_contracts/artifacts
+cp ./artifacts/* ocean_contracts/artifacts/
 abifiles=( ./ocean_contracts/artifacts/* )
 [ "${#abifiles[@]}" -lt "1" ] && echo "ABI Files for development environment not found" && exit 1
 python setup.py sdist bdist_wheel
