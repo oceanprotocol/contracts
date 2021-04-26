57f1b8e44e04c7984449aaca16ac398b08a68791
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-04-26 10:33:09 +0200

    change the artifacts folder name prior pypi publish

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -4,7 +4,9 @@ pip install --upgrade --user travis pip
 pip install --upgrade --user travis twine six==1.10.0 wheel==0.31.0 setuptools
 pip list
 shopt -s nullglob
-abifiles=( ./artifacts/* )
+mkdir -p ocean_contracts/artifacts
+mv ./artifacts/* ocean_contracts/artifacts
+abifiles=( ./ocean_contracts/artifacts/* )
 [ "${#abifiles[@]}" -lt "1" ] && echo "ABI Files for development environment not found" && exit 1
 python setup.py sdist bdist_wheel
 twine upload dist/*
