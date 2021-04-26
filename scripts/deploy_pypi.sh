7dbfc8bfd17a2593537dc4ead8f57784cabb2406
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-04-26 12:10:18 +0200

    add __init__.py

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -5,6 +5,7 @@ pip install --upgrade --user travis twine six==1.10.0 wheel==0.31.0 setuptools
 pip list
 shopt -s nullglob
 mkdir -p ocean_contracts/artifacts
+touch ocean_contracts/__init__.py
 cp ./artifacts/* ocean_contracts/artifacts/
 abifiles=( ./ocean_contracts/artifacts/* )
 [ "${#abifiles[@]}" -lt "1" ] && echo "ABI Files for development environment not found" && exit 1
