453a4bc5db0f07b6c8aa859a79d20f21bbff9102
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-04-28 12:45:46 +0200

    v0.5.20

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/ocean-contracts',
-    version='0.5.19',
+    version='0.5.20',
     zip_safe=False,
 )
