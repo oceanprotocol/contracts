8754c5f56df2a69af91f306c038c172dc54f2696
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-05-28 14:06:50 +0200

    add aurora test network

--- truffle.js
+++ truffle.js
@@ -112,6 +112,12 @@ module.exports = {
             gas: 7 * 1000000,
             gasPrice: utils.toWei('8', 'gwei')
         },
+	aurora: {
+      	    provider: () => setupWallet('https://testnet.aurora.dev'),
+            network_id: 0x4e454153,
+            gas: 10000000,
+            from: '0x6A33382de9f73B846878a57500d055B981229ac4'
+        },
         // pacific the ethereum mainnet
         pacific: {
             provider: () => setupWallet('https://pacific.oceanprotocol.com'),
