cfe671540a0061a47b4b9d3dd0c257536ab722dc
lacoop6tu <burzi.andrea@gmail.com>
2021-06-09 12:13:25 +0200

    add multiple poolIds support when collecting fees from assetManager

--- contracts/customPool/OceanManager.sol
+++ contracts/customPool/OceanManager.sol
@@ -5,7 +5,6 @@ import "../interfaces/IERC20.sol";
 import "../interfaces/IVault.sol";
 import "../interfaces/IWeightedPool.sol";
 
-
 contract OceanManager {
     address public owner;
     address public vault;
@@ -13,7 +12,11 @@ contract OceanManager {
     address public communityFC;
     mapping(address => uint256) public feesCollected;
 
-    constructor(address _owner, address _vault, address _communityFC) {
+    constructor(
+        address _owner,
+        address _vault,
+        address _communityFC
+    ) {
         owner = _owner;
         vault = _vault;
         communityFC = _communityFC;
@@ -32,73 +35,72 @@ contract OceanManager {
         return array.length;
     }
 
-  
     IVault.PoolBalanceOp[] public withdraws;
     IVault.PoolBalanceOp[] public updates;
 
     function _manageAndUpdatePoolBalanceOcean(
-        bytes32 poolId,
+        bytes32[] memory poolIds,
         IVault.AssetManagerTransfer[] memory transfers
     ) internal {
         // USE TOKEN LENTGH FROM POOL ID or use lentgh from collectFee and pass it as argument
+        for (uint256 j = 0; j < getPoolsLength(poolIds); j++) {
+            
+            for (uint256 i = 0; i < getLength(transfers); i++) {
+                IVault.PoolBalanceOp memory transfer =
+                    IVault.PoolBalanceOp(
+                        IVault.PoolBalanceOpKind.WITHDRAW,
+                        poolIds[i],
+                        transfers[i].token,
+                        transfers[i].amount
+                    );
 
-        for (uint256 i = 0; i < getLength(transfers); i++) {
-            IVault.PoolBalanceOp memory transfer =
-                IVault.PoolBalanceOp(
-                    IVault.PoolBalanceOpKind.WITHDRAW,
-                    poolId,
-                    transfers[i].token,
-                    transfers[i].amount
-                );
+                withdraws.push(transfer);
+            }
+            IVault(vault).managePoolBalance(withdraws);
+            delete withdraws;
 
-            withdraws.push(transfer);
+            for (uint256 i = 0; i < getLength(transfers); i++) {
+                IVault.PoolBalanceOp memory update =
+                    IVault.PoolBalanceOp(
+                        IVault.PoolBalanceOpKind.UPDATE,
+                        poolIds[i],
+                        transfers[i].token,
+                        0 // We report it as 'full losses'
+                    );
+
+                updates.push(update);
+            }
+            IVault(vault).managePoolBalance(updates);
+            delete updates;
         }
-        IVault(vault).managePoolBalance(withdraws);
-        delete withdraws;
-        
-        for (uint256 i = 0; i < getLength(transfers); i++) {
-            IVault.PoolBalanceOp memory update =
-                IVault.PoolBalanceOp(
-                    IVault.PoolBalanceOpKind.UPDATE,
-                    poolId,
-                    transfers[i].token,
-                    0 // We report it as 'full losses'
-                );
-
-            updates.push(update);
-        }
-        IVault(vault).managePoolBalance(updates);
-        delete updates;
         // At this stage all balances are in the Asset Manager
         // We should then add a function to transfer them to the OPF Community Collector
     }
 
     // TODO? SHOULD WE CREATE A BATCH FUNCTION FOR COLLECTING FEES FROM MULTIPLE POOLS?
     function collectFee(
-        bytes32 poolId,
+        bytes32[] memory poolIds,
         IVault.AssetManagerTransfer[] memory transfers
     ) external onlyOwner {
         // TODO?: GET NUMBER OF TOKEN FROM POOL ID and create the transfers(IVault.AssetManagerTransfer[]) during the loop (more costly!)
+        for (uint256 j = 0; j < getPoolsLength(poolIds); j++) {
+            for (uint256 i = 0; i < getLength(transfers); i++) {
+                uint256 totalFee =
+                    IWeightedPool(pool).communityFees(
+                        address(transfers[i].token)
+                    );
+                uint256 actualFee =
+                    totalFee - feesCollected[address(transfers[i].token)];
 
-        for (uint256 i = 0; i < getLength(transfers); i++) {
-            uint256 totalFee =
-                IWeightedPool(pool).communityFees(
-                    address(transfers[i].token)
-                );
-            uint256 actualFee =
-                totalFee - feesCollected[address(transfers[i].token)];
-            
-            feesCollected[address(transfers[i].token)] =
-                feesCollected[address(transfers[i].token)] +
-                actualFee;
+                feesCollected[address(transfers[i].token)] =
+                    feesCollected[address(transfers[i].token)] +
+                    actualFee;
 
-            transfers[i].amount = actualFee;
-
-            
-            
+                transfers[i].amount = actualFee;
+            }
         }
 
-        _manageAndUpdatePoolBalanceOcean(poolId, transfers);
+        _manageAndUpdatePoolBalanceOcean(poolIds, transfers);
     }
 
     function getTokensLength(IERC20[] memory array)
@@ -108,13 +110,19 @@ contract OceanManager {
     {
         return array.length;
     }
+
+    function getPoolsLength(bytes32[] memory array)
+        internal
+        view
+        returns (uint256)
+    {
+        return array.length;
+    }
+
     // We could do it this way or deciding to batch swap into a single token(OCEAN? Stablecoin?) and then send it to the community collector
     function transferToOPFCollector(IERC20[] memory tokens) external {
-
         for (uint256 i = 0; i < getTokensLength(tokens); i++) {
             tokens[i].transfer(communityFC, tokens[i].balanceOf(address(this)));
         }
     }
-
-   
 }
