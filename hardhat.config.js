d0019b834d6a48b9db1df18b9457ab5ae472ede0
alexcos20 <alex.coseru@gmail.com>
2021-10-11 23:37:04 -0700

    add gas usage plugin

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-contract-sizer");
+require("hardhat-gas-reporter");
 require('solidity-coverage');
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -77,4 +78,8 @@ module.exports = {
     runOnCompile: true,
     disambiguatePaths: false,
   },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 80
+  }
 };
