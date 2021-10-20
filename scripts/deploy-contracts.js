5916615fc4963364b770ff844feceb2cd7e5a77a
alexcos20 <alex.coseru@gmail.com>
2021-10-20 02:51:36 -0700

    fix deployments

--- scripts/deploy-contracts.js
+++ scripts/deploy-contracts.js
@@ -17,12 +17,16 @@ const shouldDeployOPFCommunity = true
 const logging = true
 async function main() {
   const url = process.env.NETWORK_RPC_URL;
+  console.log(url)
   if (!url) {
     console.error("Missing NETWORK_RPC_URL. Aborting..");
     return null;
   }
   const provider = new ethers.providers.JsonRpcProvider(url);
   const network = provider.getNetwork()
+  // utils
+  const networkDetails = await network
+  
   let wallet
   if (process.env.MNEMONIC)
     wallet = new Wallet.fromMnemonic(process.env.MNEMONIC)
@@ -63,8 +67,6 @@ async function main() {
   if (logging)
     console.info("Use existing addresses:" + JSON.stringify(addresses, null, 2))
 
-  // utils
-  const networkDetails = await network
   addresses.chainId = networkDetails.chainId
   if (shouldDeployOceanMock) {
     if (logging) console.info("Deploying OceanMock")
@@ -80,24 +82,19 @@ async function main() {
 
   if (logging) console.info("Deploying V4 contracts")
   // v4 contracts
-  const FixedPriceExchange = await ethers.getContractFactory(
-    "FixedRateExchange"
-  );
+  
 
-  const ERC721Template = await ethers.getContractFactory("ERC721Template");
-  const ERC20Template = await ethers.getContractFactory("ERC20Template");
-  const ERC721Factory = await ethers.getContractFactory("ERC721Factory");
-
-  const Router = await ethers.getContractFactory("FactoryRouter");
-  const SSContract = await ethers.getContractFactory("SideStaking");
-  const BPool = await ethers.getContractFactory("BPool");
-  const Dispenser = await ethers.getContractFactory("Dispenser");
-
-  const poolTemplate = await BPool.deploy();
-
+  
+  
   // DEPLOY ROUTER, SETTING OWNER
 
+  if (logging) console.info("Deploying BPool")
+  const BPool = await ethers.getContractFactory("BPool", owner);
+  const poolTemplate = await BPool.deploy();
+  addresses.poolTemplate = poolTemplate.address
+
   if (logging) console.log('Deploying Router')
+  const Router = await ethers.getContractFactory("FactoryRouter", owner);
   const router = await Router.deploy(
     owner.address,
     oceanAddress,
@@ -105,30 +102,52 @@ async function main() {
     addresses.OPFCommunityFeeCollector,
     []
   );
-  if (logging) console.info("Deploying FixedPriceExchange")
+  addresses.Router = router.address
+  
+  
+  if (logging) console.info("Deploying FixedrateExchange")
+  const FixedPriceExchange = await ethers.getContractFactory(
+    "FixedRateExchange", owner
+  );
   const fixedPriceExchange = await FixedPriceExchange.deploy(
     router.address,
     addresses.OPFCommunityFeeCollector
   );
+  addresses.FixedPrice = fixedPriceExchange.address
+
+
   if (logging) console.info("Deploying StakingContract")
+  const SSContract = await ethers.getContractFactory("SideStaking", owner);
   const ssPool = await SSContract.deploy(router.address);
+  addresses.Staking = ssPool.address
+  
+
   if (logging) console.info("Deploying ERC20 Template")
+  const ERC20Template = await ethers.getContractFactory("ERC20Template", owner);
   const templateERC20 = await ERC20Template.deploy();
+  addresses.ERC20Template = templateERC20.address
+
   if (logging) console.info("Deploying Dispenser")
+  const Dispenser = await ethers.getContractFactory("Dispenser", owner);
   const dispenser = await Dispenser.deploy(
     router.address
   );
-  if (logging) console.info("Deploying ERC721 Template")
-  const templateERC721 = await ERC721Template.deploy();
+  addresses.Dispenser = dispenser.address
 
+  if (logging) console.info("Deploying ERC721 Template")
+  const ERC721Template = await ethers.getContractFactory("ERC721Template", owner);
+  const templateERC721 = await ERC721Template.deploy();
+  addresses.ERC721Template = templateERC721.address
+  
   if (logging) console.info("Deploying ERC721 Factory")
+  const ERC721Factory = await ethers.getContractFactory("ERC721Factory", owner);
   const factoryERC721 = await ERC721Factory.deploy(
     templateERC721.address,
     templateERC20.address,
     addresses.OPFCommunityFeeCollector,
     router.address
   );
-
+  addresses.ERC721Factory = factoryERC721.address
   // SET REQUIRED ADDRESS
 
   if (logging) console.info("Adding factoryERC721.address")  
@@ -142,14 +161,10 @@ async function main() {
   if (logging) console.info("Moving Router ownership")  
   await router.connect(owner).changeRouterOwner(OPFOwner)
 
-  addresses.ERC721Factory = factoryERC721.address
-  addresses.ERC20Template = templateERC20.address
-  addresses.ERC721Template = templateERC721.address
-  addresses.Router = router.address
-  addresses.FixedPrice = fixedPriceExchange.address
-  addresses.Staking = ssPool.address
-  addresses.poolTemplate = poolTemplate.address
-
+  
+  
+  
+  
   if (addressFile) {
     // write address.json if needed
     oldAddresses[networkName] = addresses
