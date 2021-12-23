ba8dd5eb554981843b35d902fab624dc7a61dc67
alexcos20 <alex.coseru@gmail.com>
2021-12-23 03:31:06 -0800

    new rinkeby deployment + deploy script updates

--- scripts/deploy-contracts.js
+++ scripts/deploy-contracts.js
@@ -16,11 +16,16 @@ let shouldDeployOceanMock = false;
 let shouldDeployOPFCommunityFeeCollector = false;
 const shouldDeployOPFCommunity = true;
 const logging = true;
+const show_verify = true;
 async function main() {
   const url = process.env.NETWORK_RPC_URL;
-  console.log(url);
+  console.log("Using RPC: "+url);
   if (!url) {
     console.error("Missing NETWORK_RPC_URL. Aborting..");
+    return null;
+  }
+  if( !process.env.ADDRESS_FILE){
+    console.error("Missing ADDRESS_FILE. Aborting..");
     return null;
   }
   const provider = new ethers.providers.JsonRpcProvider(url);
@@ -158,7 +163,12 @@ async function main() {
       OPFOwner,
       OPFOwner
     );
+    await opfcommunityfeecollector.deployTransaction.wait();
     addresses.OPFCommunityFeeCollector = opfcommunityfeecollector.address;
+    if(show_verify){
+      console.log("\tRun the following to verify on etherscan");
+      console.log("\tnpx hardhat verify --network "+networkName+" "+opfcommunityfeecollector.address+" "+OPFOwner+" "+OPFOwner)
+    }
   }
   else {
     addresses.OPFCommunityFeeCollector = OPFCommunityFeeCollectorAddress;
@@ -173,9 +183,13 @@ async function main() {
   if (logging) console.info("Deploying BPool");
   const BPool = await ethers.getContractFactory("BPool", owner);
   const poolTemplate = await BPool.deploy();
-  await poolTemplate.deployTransaction.wait();
+  const receipt = await poolTemplate.deployTransaction.wait();
+  addresses.startBlock = receipt.blockNumber 
   addresses.poolTemplate = poolTemplate.address;
-
+  if(show_verify){
+    console.log("\tRun the following to verify on etherscan");
+    console.log("\tnpx hardhat verify --network "+networkName+" "+addresses.poolTemplate)
+  }
   if (logging) console.log("Deploying Router");
   const Router = await ethers.getContractFactory("FactoryRouter", owner);
   const router = await Router.deploy(
@@ -187,6 +201,20 @@ async function main() {
   );
   await router.deployTransaction.wait();
   addresses.Router = router.address;
+  if(show_verify){
+    console.log("\tRun the following to verify on etherscan");
+    console.log("\tcat > args1.js\n");
+    console.log("\tmodule.exports=[\n");
+    console.log("\t'"+owner.address+"',\n'");
+    console.log("\t'"+addresses.Ocean+"',\n'");
+    console.log("\t'"+poolTemplate.address+"',\n'");
+    console.log("\t'"+addresses.OPFCommunityFeeCollector+"',\n'");
+    console.log("\t[]\n'");
+    console.log("\t];");
+    console.log("\tCTRL+D");
+    console.log("\tnpx hardhat verify --network "+networkName+" -constructor-args args1.js "+addresses.Router+" "+owner.address+" "+addresses.Ocean
+    +" "+poolTemplate.address+" "+addresses.OPFCommunityFeeCollector+" []")
+  }
 
   if (logging) console.info("Deploying FixedrateExchange");
   const FixedPriceExchange = await ethers.getContractFactory(
@@ -199,18 +227,28 @@ async function main() {
   );
   await fixedPriceExchange.deployTransaction.wait();
   addresses.FixedPrice = fixedPriceExchange.address;
-
+  if(show_verify){
+    console.log("\tRun the following to verify on etherscan");
+    console.log("\tnpx hardhat verify --network "+networkName+" "+addresses.FixedPrice+" "+router.address+" "+addresses.OPFCommunityFeeCollector)
+  }
   if (logging) console.info("Deploying StakingContract");
   const SSContract = await ethers.getContractFactory("SideStaking", owner);
   const ssPool = await SSContract.deploy(router.address);
   await ssPool.deployTransaction.wait();
   addresses.Staking = ssPool.address;
-
+  if(show_verify){
+    console.log("\tRun the following to verify on etherscan");
+    console.log("\tnpx hardhat verify --network "+networkName+" "+addresses.Staking+" "+router.address)
+  }
   addresses.ERC20Template = {};
   if (logging) console.info("Deploying ERC20 Template");
   const ERC20Template = await ethers.getContractFactory("ERC20Template", owner);
   const templateERC20 = await ERC20Template.deploy();
   await templateERC20.deployTransaction.wait();
+  if(show_verify){
+    console.log("\tRun the following to verify on etherscan");
+    console.log("\tnpx hardhat verify --network "+networkName+" "+templateERC20.address)
+  }
   if (logging) console.info("Deploying ERC20 Enterprise Template");
   const ERC20TemplateEnterprise = await ethers.getContractFactory(
     "ERC20TemplateEnterprise",
@@ -218,6 +256,10 @@ async function main() {
   );
   const templateERC20Enterprise = await ERC20TemplateEnterprise.deploy();
   await templateERC20Enterprise.deployTransaction.wait();
+  if(show_verify){
+    console.log("\tRun the following to verify on etherscan");
+    console.log("\tnpx hardhat verify --network "+networkName+" "+templateERC20Enterprise.address)
+  }
   addresses.ERC721Template = {};
   if (logging) console.info("Deploying ERC721 Template");
   const ERC721Template = await ethers.getContractFactory(
@@ -226,12 +268,19 @@ async function main() {
   );
   const templateERC721 = await ERC721Template.deploy();
   await templateERC721.deployTransaction.wait();
+  if(show_verify){
+    console.log("\tRun the following to verify on etherscan");
+    console.log("\tnpx hardhat verify --network "+networkName+" "+templateERC721.address)
+  }
   if (logging) console.info("Deploying Dispenser");
   const Dispenser = await ethers.getContractFactory("Dispenser", owner);
   const dispenser = await Dispenser.deploy(router.address);
   await dispenser.deployTransaction.wait();
   addresses.Dispenser = dispenser.address;
-
+  if(show_verify){
+    console.log("\tRun the following to verify on etherscan");
+    console.log("\tnpx hardhat verify --network "+networkName+" "+dispenser.address+" "+router.address)
+  }
   if (logging) {
     const params = {
 
@@ -251,6 +300,10 @@ async function main() {
     router.address
   );
   await factoryERC721.deployTransaction.wait();
+  if(show_verify){
+    console.log("\tRun the following to verify on etherscan");
+    console.log("\tnpx hardhat verify --network "+networkName+" "+factoryERC721.address+" "+templateERC721.address+" "+templateERC20.address+" "+addresses.OPFCommunityFeeCollector+" "+router.address)
+  }
   addresses.ERC721Factory = factoryERC721.address;
   const nftCount = await factoryERC721.getCurrentNFTTemplateCount();
   const nftTemplate = await factoryERC721.getNFTTemplate(nftCount);
@@ -259,14 +312,14 @@ async function main() {
   let currentTokenCount = await factoryERC721.getCurrentTemplateCount();
   let tokenTemplate = await factoryERC721.getTokenTemplate(currentTokenCount);
   addresses.ERC20Template[currentTokenCount.toString()] = templateERC20.address;
-
   if (logging) console.info("Adding ERC20Enterprise to ERC721Factory");
-  await factoryERC721.addTokenTemplate(templateERC20Enterprise.address);
+  const templateadd = await factoryERC721.addTokenTemplate(templateERC20Enterprise.address);
+  await templateadd.wait();
   currentTokenCount = await factoryERC721.getCurrentTemplateCount();
   tokenTemplate = await factoryERC721.getTokenTemplate(currentTokenCount);
   addresses.ERC20Template[currentTokenCount.toString()] =
     templateERC20Enterprise.address;
-
+    
   // SET REQUIRED ADDRESS
 
   if (logging) console.info("Adding factoryERC721.address");
