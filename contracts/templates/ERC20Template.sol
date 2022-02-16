650e46a19c0ba3de675b42f60e6c546a50d1a043
lacoop6tu <burzi.andrea@gmail.com>
2022-02-16 17:13:57 -0500

    fix zero address warnings

--- contracts/templates/ERC20Template.sol
+++ contracts/templates/ERC20Template.sol
@@ -822,6 +822,10 @@ contract ERC20Template is
             _publishMarketFeeAddress != address(0),
             "Invalid _publishMarketFeeAddress address"
         );
+           require(
+            _publishMarketFeeToken != address(0),
+            "Invalid _publishMarketFeeToken address"
+        );
         publishMarketFeeAddress = _publishMarketFeeAddress;
         publishMarketFeeToken = _publishMarketFeeToken;
         publishMarketFeeAmount = _publishMarketFeeAmount;
