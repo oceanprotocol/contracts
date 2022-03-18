6907a7e7a5bfb215ba5bdf96e8fab75c6bd4a60c
alexcos20 <alex.coseru@gmail.com>
2022-03-17 21:52:57 -0700

    fix #569

--- contracts/utils/ERC20Roles.sol
+++ contracts/utils/ERC20Roles.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.12;
 
 contract ERC20Roles {
     
-   
+    
     mapping(address => RolesERC20) public permissions;
 
     address[] public authERC20;
@@ -28,24 +28,40 @@ contract ERC20Roles {
         uint256 blockNumber
     );
 
+    /**
+    * @dev getPermissions
+    *      Returns list of roles for an user
+    * @param user user address
+    */
     function getPermissions(address user) public view returns (RolesERC20 memory) {
         return permissions[user];
     }
 
+    /**
+    * @dev _addMinter
+    *      Internal function to add minter role to an user.
+    * @param _minter user address
+    */
     function _addMinter(address _minter) internal {
         if(_minter != address(0)){
             RolesERC20 storage user = permissions[_minter];
             require(user.minter == false, "ERC20Roles:  ALREADY A MINTER");
             user.minter = true;
-            authERC20.push(_minter);
+            pushToAuthERC20(_minter);
             emit AddedMinter(_minter,msg.sender,block.timestamp,block.number);
         }
     }
 
+    /**
+    * @dev _removeMinter
+    *      Internal function to remove minter role from an user.
+    * @param _minter user address
+    */
     function _removeMinter(address _minter) internal {
         RolesERC20 storage user = permissions[_minter];
         user.minter = false;
         emit RemovedMinter(_minter,msg.sender,block.timestamp,block.number);
+        SafeRemoveFromAuthERC20(_minter);
     }
 
     event AddedPaymentManager(
@@ -60,20 +76,32 @@ contract ERC20Roles {
         uint256 timestamp,
         uint256 blockNumber
     );
+
+    /**
+    * @dev _addPaymentManager
+    *      Internal function to add paymentManager role to an user.
+    * @param _paymentCollector user address
+    */
     function _addPaymentManager(address _paymentCollector) internal {
         if(_paymentCollector != address(0)){
             RolesERC20 storage user = permissions[_paymentCollector];
             require(user.paymentManager == false, "ERC20Roles:  ALREADY A FEE MANAGER");
             user.paymentManager = true;
-            authERC20.push(_paymentCollector);
+            pushToAuthERC20(_paymentCollector);
             emit AddedPaymentManager(_paymentCollector,msg.sender,block.timestamp,block.number);
         }
     }
 
+    /**
+    * @dev _removePaymentManager
+    *      Internal function to remove paymentManager role from an user.
+    * @param _paymentCollector user address
+    */
     function _removePaymentManager(address _paymentCollector) internal {
         RolesERC20 storage user = permissions[_paymentCollector];
         user.paymentManager = false;
         emit RemovedPaymentManager(_paymentCollector,msg.sender,block.timestamp,block.number);
+        SafeRemoveFromAuthERC20(_paymentCollector);
     }
 
 
@@ -101,5 +129,43 @@ contract ERC20Roles {
         
     }
 
+
+
+        /**
+    * @dev pushToAuthERC20
+    *      Checks authERC20 array and adds the user address if does not exists
+    * @param user address to be checked
+    */
+    function pushToAuthERC20(address user) internal {
+        uint256 i;
+        for (i = 0; i < authERC20.length; i++) {
+            if(authERC20[i] == user) break;
+        }
+        if(i == authERC20.length){
+            // element was not found
+            authERC20.push(user);
+        }
+    }
+
+    /**
+    * @dev SafeRemoveFromAuthERC20
+    *      Checks if user has any roles left, and if not, it will remove it from auth array
+    * @param user address to be checked and removed
+    */
+    function SafeRemoveFromAuthERC20(address user) internal {
+        RolesERC20 storage userRoles = permissions[user];
+        if (userRoles.minter == false &&
+            userRoles.paymentManager == false
+        ){
+            uint256 i;
+            for (i = 0; i < authERC20.length; i++) {
+                if(authERC20[i] == user) break;
+            }
+            if(i < authERC20.length){
+                authERC20[i] = authERC20[authERC20.length -1];
+                authERC20.pop();
+            }
+        }
+    }
     
 }
