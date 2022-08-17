f8af4cd098dd901674e522b8fdc8164cb3dff3ef
Berkay Saglam <25263018+trizin@users.noreply.github.com>
2022-08-17 11:54:28 +0300

    Update `veAllocate`

--- contracts/ve/veAllocate.sol
+++ contracts/ve/veAllocate.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.12;
 // Code is Apache-2.0 and docs are CC-BY-4.0
 
 contract veAllocate {
-    mapping(address => mapping(address => mapping(uint256 => uint256))) private veAllocation;
+    mapping(address => mapping(address => mapping(uint256 => uint256)))
+        private veAllocation;
     mapping(address => uint256) private _totalAllocation;
 
     event AllocationSet(
@@ -14,11 +15,11 @@ contract veAllocate {
         uint256 amount
     );
 
-    function getveAllocation(address _address, address _nft, uint256 chainid)
-        public
-        view
-        returns (uint256)
-    {
+    function getveAllocation(
+        address _address,
+        address _nft,
+        uint256 chainid
+    ) public view returns (uint256) {
         return veAllocation[_address][_nft][chainid];
     }
 
@@ -30,19 +31,17 @@ contract veAllocate {
         return _totalAllocation[_address];
     }
 
-
     function setAllocation(
         uint256 amount,
         address nft,
         uint256 chainId
     ) external {
-        require(amount <= 1000, "BM");
-
         _totalAllocation[msg.sender] =
             _totalAllocation[msg.sender] +
             amount -
             veAllocation[msg.sender][nft][chainId];
 
+        require(_totalAllocation[msg.sender] <= 1000, "Max Allocation");
         veAllocation[msg.sender][nft][chainId] = amount;
         emit AllocationSet(msg.sender, nft, chainId, amount);
     }
