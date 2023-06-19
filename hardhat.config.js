a81ca1004f46e3667f2a0560063d447d11cbb692
alexcos20 <alex.coseru@gmail.com>
2023-06-18 23:43:31 -0700

    v1.1.13

--- hardhat.config.js
+++ hardhat.config.js
@@ -72,6 +72,12 @@ module.exports = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    sepolia: {
+      url:
+        process.env.NETWORK_RPC_URL !== undefined ? process.env.NETWORK_RPC_URL : "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     mumbai: {
       url:
         process.env.NETWORK_RPC_URL !== undefined ? process.env.NETWORK_RPC_URL : "",
