ed2bee7893c1504e3d6d41752b2878c62e3d4728
mamjadmm <137369015+mamjadmm@users.noreply.github.com>
2023-07-02 15:22:28 +0500

    Revert "Add Sepolia"

--- hardhat.config.js
+++ hardhat.config.js
@@ -72,12 +72,6 @@ module.exports = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    sepolia: {
-      url:
-        process.env.NETWORK_RPC_URL !== undefined ? process.env.NETWORK_RPC_URL : "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
     mumbai: {
       url:
         process.env.NETWORK_RPC_URL !== undefined ? process.env.NETWORK_RPC_URL : "",
