054d78cd1632aed587433639747c38217b5c47ec
alexcos20 <alex.coseru@gmail.com>
2023-08-10 08:36:17 -0700

    do not delete artifacts

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -7,10 +7,6 @@ echo "deploy contracts is ${DEPLOY_CONTRACTS}"
 
 if [ "${DEPLOY_CONTRACTS}" = "true" ]
 then
-    sleep 2
-    # remove ready flag if we deploy contracts
-    rm -f /ocean-contracts/artifacts/ready
-    
     #we have to sleep until ganache is ready
     sleep ${SLEEP_FOR_GANACHE}
     cp hardhat.config.barge.js hardhat.config.js
