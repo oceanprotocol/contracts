02d4cdafde174fe12f01531f44898dd81ddc1712
alexcos20 <alex.coseru@gmail.com>
2023-09-07 05:47:11 -0700

    devops updates

---
+++ scripts/check_deployment.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+const fs = require('fs')
+
+async function testDeployment() {
+  // load addresses file first
+  addresses = null
+  console.log("Loading "+process.env.ADDRESS_FILE)
+  try {
+    addresses = JSON.parse(fs.readFileSync(process.env.ADDRESS_FILE, 'utf8'))
+  }
+  catch {
+    console.error("Could not load addreses files")
+    process.exit(1)
+  }
+  if (!('development' in addresses)) {
+    console.error("Missing development network")
+    process.exit(1)
+  }
+  keys = ["startBlock", "Router", "FixedPrice", "ERC20Template", "ERC721Template", "Dispenser", "ERC721Factory"]
+  for (const key of keys) {
+    if (!(key in addresses['development'])) {
+      console.error("Missing " + key + " deployment")
+      process.exit(1)
+    }
+  }
+  console.log("All good")
+  process.exit(0)
+}
+
+testDeployment()
