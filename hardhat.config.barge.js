d143e5b01d5e74be982d53a7d4fa8729b50304a8
alexcos20 <alex.coseru@gmail.com>
2024-06-28 15:56:02 +0300

    add accesslists contracts

--- hardhat.config.barge.js
+++ hardhat.config.barge.js
@@ -33,6 +33,15 @@ module.exports = {
           },
         },
       },
+      {
+        version: "0.8.26",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
 
     ],
     overrides: {},
--- hardhat.config.js
+++ hardhat.config.js
@@ -32,6 +32,15 @@ module.exports = {
           },
         },
       },
+      {
+        version: "0.8.26",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
 
     ],
     overrides: {},
