ba56e452037ed9eed48645cbc29ff0f576e09c54
alexcos20 <alex.coseru@gmail.com>
2024-06-28 16:02:32 +0300

    remove debug code

--- contracts/accesslists/AccessList.sol
+++ contracts/accesslists/AccessList.sol
@@ -11,8 +11,6 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "hardhat/console.sol";
-
 /**
  * @title AccessList
  *
