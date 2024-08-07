ede0a1ec1a867699cd863e9e7bd0a82496b6e9c1
alexcos20 <alex.coseru@gmail.com>
2024-08-07 18:10:28 +0300

    fix deploy script

--- scripts/deploy-contracts.js
+++ scripts/deploy-contracts.js
@@ -524,18 +524,18 @@ async function main() {
     
     if (sleepAmount > 0) await sleep(sleepAmount)
     if (logging) console.info("Deploying ERC20 Sapphire Template");
-      const ERC20TemplateSapphire = await ethers.getContractFactory(
+    const ERC20TemplateSapphire = await ethers.getContractFactory(
         "ERC20TemplateSapphire",
         owner
       );
-      let templateERC20Sapphire
-      if (options) templateERC20Sapphire = await ERC20TemplateSapphire.connect(owner).deploy(options);
-      else templateERC20Sapphire = await ERC20TemplateSapphire.connect(owner).deploy();
-      await templateERC20Sapphire.deployTransaction.wait();
-      if (show_verify) {
+    let templateERC20TemplateSapphire
+    if (options) templateERC20Sapphire = await ERC20TemplateSapphire.connect(owner).deploy(options);
+    else templateERC20TemplateSapphire = await ERC20TemplateSapphire.connect(owner).deploy();
+    await templateERC20TemplateSapphire.deployTransaction.wait();
+    if (show_verify) {
         console.log("\tRun the following to verify on etherscan");
-        console.log("\tnpx hardhat verify --network " + networkName + " " + templateERC20Sapphire.address)
-      }
+        console.log("\tnpx hardhat verify --network " + networkName + " " + templateERC20TemplateSapphire.address)
+    }
 
     addresses.ERC721Template = {};
     if (sleepAmount > 0) await sleep(sleepAmount)
